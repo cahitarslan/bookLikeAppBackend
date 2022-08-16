00fe3cdc0cd1e281cc7bedda349e1b599fe2e4bc
Cahit Arslan <cahitarslan.business@gmail.com>
2022-08-16 16:54:49 +0300

    socketio entegration finished

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const socketio = require('socket.io');
 const express = require('express');
 const http = require('http');
+require('dotenv').config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -17,6 +18,15 @@ const io = socketio(server, {
 server.listen(PORT, () => {
     io.on('connection', (socket) => {
         console.log('HOPPAAAAAAAA');
-        console.log(socket);
+        console.log(socket.id);
+        //& Karşılama Mesajı Gönder...
+        // io.in(roomID).emit()
+        socket.emit('WELCOME_MESSAGE', `Oooooo ${socket.id} kardeşim ojgeldin be yaa...`);
+        socket.on('NEW_BOOKMARK_EVENT', (bookmark) => {
+            console.log('bookmark :>> ', bookmark);
+            // io.emit('NEW_BOOKMARK_ADDED', bookmark);
+            // & Gönderen Hariç Herkese bookmark bilgisini Gönder
+            socket.broadcast.emit('NEW_BOOKMARK_ADDED', bookmark);
+        });
     });
 });
